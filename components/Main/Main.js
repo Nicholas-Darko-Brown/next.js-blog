795718602a1e5c0fca2845fc4edb13f2ab6009cb
Nicholas-Darko-Brown <nicholas.brown@amalitech.com>
2022-08-04 17:57:24 +0000

    feat: created main component

---
+++ components/Main/Main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import FirstSection from '../FirstSection/FirstSection'
+import SecondSection from '../SecondSection/SecondSection'
+import ThirdSection from '../ThirdSection/ThirdSection'
+
+const Main = () => {
+  return (
+    <>
+        <FirstSection></FirstSection>
+        <SecondSection></SecondSection>
+        <ThirdSection></ThirdSection>
+    </>
+  )
+}
+
+export default Main
